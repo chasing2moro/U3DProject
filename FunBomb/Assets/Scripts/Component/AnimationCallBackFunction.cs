c360b2f14412e415cc5ddd4770e5a81c668bff0b
chasing2moro <liboxiang@liboxiangdeMacBook-Pro.local>
2014-06-28 10:29:33 +0800

    1
    1

---
+++ FunBomb/Assets/Scripts/Component/AnimationCallBackFunction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationCallBackFunction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ReturnToPool(){
+		Debug.Log("AnimationCallBackFunction ReturnToPool()");
+		ExplosionPool.Instance.EnqueueToPool(this.gameObject);
+	}
+}
