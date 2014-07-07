5c35a92ec475483360cc60a9d4c0e94bffac885b
chasing2moro <730244945@qq.com>
2014-07-07 20:01:57 +0800

    1
    1

--- FunBomb/Assets/Scripts/Component/Explosion/ExplosionCallBack.cs
+++ FunBomb/Assets/Scripts/Component/Explosion/ExplosionCallBack.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AnimationCallBackFunction : MonoBehaviour {
+public class ExplosionCallBack : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
